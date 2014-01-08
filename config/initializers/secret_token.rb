bb3392e0164bf7002bb362e35dd48aa9f255f7d5
Brad Hubbard <brad@bradhubbard.net>
2014-01-08 10:39:07 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CesScorecard::Application.config.secret_key_base = 'd4b756757cc1cd0835011e6c5b17a4dc697f3b2a74603626f38568886fd910e6c6bdd989056a64c43dacee131273b0de56fb1d2cecc540696dc32e78545f66e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CesScorecard::Application.config.session_store :cookie_store, key: '_ces-scorecard_session'
