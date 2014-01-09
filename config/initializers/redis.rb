74a213579677448e7486484798923743094effaf
Brad Hubbard <brad@bradhubbard.net>
2014-01-08 22:22:41 -0800

    More Redis junk

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(URI.encode(ENV["REDISCLOUD_URL"])
+uri = URI.parse(URI.encode(ENV["REDISCLOUD_URL"]))
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
